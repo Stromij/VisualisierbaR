05d1af28f2e78b83b96d06a51a30434f8bbed4d5
Björn Petersen <pheasn@gmail.com>
2016-11-11 12:23:05 +0100

    Add logging configuration

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/DBVisualizer.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/DBVisualizer.java
@@ -3,7 +3,22 @@ package com.github.bachelorpraktikum.dbvisualization;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class DBVisualizer extends Application {
+    static {
+        try {
+            LogManager.getLogManager().readConfiguration(DBVisualizer.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.out.println("Can't initialize logging!");
+            System.exit(100);
+        }
+    }
+
+    private static final Logger log = Logger.getLogger(DBVisualizer.class.getName());
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.show();
