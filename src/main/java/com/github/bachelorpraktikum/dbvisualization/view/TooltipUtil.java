95a6a3fe10ad397f4037fb90358fcbe035371b8a
Björn Petersen <pheasn@gmail.com>
2016-12-18 17:02:40 +0100

    Wait for 300ms to hide tooltip after mouse exit

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/TooltipUtil.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/TooltipUtil.java
@@ -1,19 +1,30 @@
 package com.github.bachelorpraktikum.dbvisualization.view;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
 
 public final class TooltipUtil {
     private TooltipUtil() {
     }
 
     public static void install(Node node, Tooltip tooltip) {
+        Timeline waitForHide = new Timeline(new KeyFrame(
+                Duration.millis(300),
+                ae -> tooltip.hide()
+        ));
+
         node.setOnMouseEntered(event -> {
+            waitForHide.stop();
             Bounds bounds = node.localToScreen(node.getBoundsInLocal());
             tooltip.show(node, bounds.getMinX(), bounds.getMaxY());
         });
 
-        node.setOnMouseExited(event -> tooltip.hide());
+        node.setOnMouseExited(event -> {
+            waitForHide.play();
+        });
     }
 }
