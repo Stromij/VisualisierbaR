f85603920900fa1877166cb33fa7eb49b9bf0b8e
Jonas <jonas.stromberg@stud.tu-darmstadt.de>
2018-06-09 12:00:31 +0200

    Completed syntaxhighlighting

--- src/main/java/com/github/bachelorpraktikum/visualisierbar/abslexer/SyntaxLexer.java
+++ src/main/java/com/github/bachelorpraktikum/visualisierbar/abslexer/SyntaxLexer.java
@@ -2,7 +2,6 @@ package com.github.bachelorpraktikum.visualisierbar.abslexer;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 import javax.swing.text.*;
@@ -41,9 +40,11 @@ public class SyntaxLexer {
             case AbsLexer.INTLITERAL:
                 return getLiteralsStyle();
             case AbsLexer.TYPE_IDENTIFIER:
-                return getDeclarationStyle();
+                return null;
             case AbsLexer.STRINGLITERAL:
                 return getStringStyle();
+            case AbsLexer.IDENTIFIER:
+                return null;
             case AbsLexer.T__0:                 // ?
                 return null;
             case AbsLexer.T__1:                 // "," in Parameterlisten
@@ -74,10 +75,150 @@ public class SyntaxLexer {
                 return getDeclarationStyle();
             case AbsLexer.T__14:                // "if"
                 return getDeclarationStyle();
-            case AbsLexer.T__15:
+            case AbsLexer.T__15:                // ?
+                return null;
+            case AbsLexer.T__16:                // ?
+                return null;
+            case AbsLexer.T__17:                // "case"
                 return getDeclarationStyle();
+            case AbsLexer.T__18:                // "{"
+                return  null;
+            case AbsLexer.T__19:                // "}"
+                return null;
+            case AbsLexer.T__20:                // ?
+                return null;
+            case AbsLexer.T__21:                // "=" Zuweisungsoperator
+                return null;
+            case AbsLexer.T__22:                // ?
+                return null;
+            case AbsLexer.T__23:                // "=>" Operator
+                return null;
+            case AbsLexer.T__24:                // ";"
+                return null;
+            case AbsLexer.T__25:                // "_" in "_ => skip" - Klausel
+                return null;
+            case AbsLexer.T__26:                //  ":" in [HTTP:
+                return null;
+            case AbsLexer.T__27:                // "skip"
+                return getDeclarationStyle();
+            case AbsLexer.T__28:                // "return
+                return getDeclarationStyle();
+            case AbsLexer.T__29:                // ?
+                return null;
+            case AbsLexer.T__30:                // ?
+                return null;
+            case AbsLexer.T__31:                // ?
+                return null;
+            case AbsLexer.T__32:                // ?
+                return null;
+            case AbsLexer.T__33:                // ?
+                return null;
+            case AbsLexer.T__34:                // ?
+                return null;
+            case AbsLexer.T__35:                // ?
+                return null;
+            case AbsLexer.T__36:                // "duration"
+                return getDeclarationStyle();
+            case AbsLexer.T__37:                // ?
+                return null;
+            case AbsLexer.T__38:                // ?
+                return null;
+            case AbsLexer.T__39:                // ?
+                return null;
+            case AbsLexer.T__40:                // ?
+                return null;
+            case AbsLexer.T__41:                // ?
+                return null;
+            case AbsLexer.T__42:                // ?
+                return null;
+            case AbsLexer.T__43:                // ?
+                return null;
+            case AbsLexer.T__44:                // ?
+                return null;
+            case AbsLexer.T__45:                // ?
+                return null;
+            case AbsLexer.T__46:                // ?
+                return null;
+            case AbsLexer.T__47:                // ?
+                return null;
+            case AbsLexer.T__48:                // "interface"
+                return getDeclarationStyle();
+            case AbsLexer.T__49:                // ?
+                return null;
+            case AbsLexer.T__50:                // "class"
+                return getDeclarationStyle();
+            case AbsLexer.T__51:                // "implements"
+                return getDeclarationStyle();
+            case AbsLexer.T__52:                // ?
+                return null;
             case AbsLexer.T__53:                // "module"
                 return getDeclarationStyle();
+            case AbsLexer.T__54:                // "export"
+                return getDeclarationStyle();
+            case AbsLexer.T__55:                // "from" in "export * from ..."
+                return getFromStyle();
+            case AbsLexer.T__56:                // "import"
+                return getDeclarationStyle();
+            case AbsLexer.T__57:                // ?
+                return null;
+            case AbsLexer.T__58:                // ?
+                return null;
+            case AbsLexer.T__59:                // "adds"
+                return getDeclarationStyle();
+            case AbsLexer.T__60:                // "modifies"
+                return getDeclarationStyle();
+            case AbsLexer.T__61:                // ?
+                return null;
+            case AbsLexer.T__62:                // "delta"
+                return getDeclarationStyle();
+            case AbsLexer.T__63:                // ?
+                return null;
+            case AbsLexer.T__64:                // ?
+                return null;
+            case AbsLexer.T__65:                // ?
+                return null;
+            case AbsLexer.T__66:                // ?
+                return null;
+            case AbsLexer.T__67:                // ?
+                return null;
+            case AbsLexer.T__68:                // ?
+                return null;
+            case AbsLexer.T__69:                // "productline"
+                return getDeclarationStyle();
+            case AbsLexer.T__70:                // "features"
+                return getDeclarationStyle();
+            case AbsLexer.T__71:                // ?
+                return null;
+            case AbsLexer.T__72:                // "after"
+                return getDeclarationStyle();
+            case AbsLexer.T__73:                // "when"
+                return getDeclarationStyle();
+            case AbsLexer.T__74:                // ?
+                return null;
+            case AbsLexer.T__75:                // "product"
+                return getDeclarationStyle();
+            case AbsLexer.T__76:                // "group"
+                return getDeclarationStyle();
+            case AbsLexer.T__77:                // "oneof"
+                return getFromStyle();
+            case AbsLexer.T__78:                // ?
+                return null;
+            case AbsLexer.T__79:                // ?
+                return null;
+            case AbsLexer.T__80:                // ?
+                return null;
+            case AbsLexer.T__81:                // ?
+                return null;
+            case AbsLexer.T__82:                // ?
+                return null;
+            case AbsLexer.T__83:                // ?
+                return null;
+            case AbsLexer.T__84:                // ?
+                return null;
+            case AbsLexer.T__85:                // "root"
+                return getDeclarationStyle();
+            case AbsLexer.T__86:                // ?
+                return null;
             case 90:                            // Leerzeichen
                 return null;
 
@@ -100,12 +241,6 @@ public class SyntaxLexer {
          return declarationStyle;
         }
 
-    private Style getFunctionStyle()
-        {Style functionStyle = new StyleContext().addStyle("function", null);
-         StyleConstants.setForeground(functionStyle, new Color(255, 139, 0));
-         return functionStyle;
-        }
-
     private Style getLiteralsStyle()
         {Style literalStyle = new StyleContext().addStyle("literal", null);
          StyleConstants.setForeground(literalStyle, new Color(203, 0, 5));
@@ -119,10 +254,9 @@ public class SyntaxLexer {
          return stringStyle;
         }
 
-    private Style getTestStyle()
-        {Style testStyle = new StyleContext().addStyle("test", null);
-         StyleConstants.setForeground(testStyle, new Color(255, 0, 209));
-         StyleConstants.setBold(testStyle, true);
-         return testStyle;
+    private Style getFromStyle()
+        {Style stringStyle = new StyleContext().addStyle("from", null);
+         StyleConstants.setItalic(stringStyle, true);
+         return stringStyle;
         }
 }
